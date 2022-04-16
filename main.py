4f8e7fc5d1a3f620430966e40584967295cc830b
TechnoDotΩ <73961295+TechDudie@users.noreply.github.com>
2022-04-16 09:24:43 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
+import urllib.request
 import sys
 import os
+from lib.scanner import *
+
 def error(message):
-  print(message)
+  print(f"{message} Aborting...")
   exit()
 try:
   link = sys.argv[1]
 except:
-  error("Git link not provided. Aborting...")
+  error("GitHub link not provided.")
+
+parts = link.split("/")
+username, repo = urlscan(parts)
+validation = urllib.request.urlopen(f"https://github.com/repos/{username}/{repo}")
+if validation.getcode() !== 200:
+  error("Invalid GitHub URL.")
+print(validation.read())
